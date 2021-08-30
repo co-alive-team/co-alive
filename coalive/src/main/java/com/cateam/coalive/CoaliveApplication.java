f6ea6175634248040690940c78adae6ecc2742a4
wyjax <wyjax.dev@gmail.com>
2021-08-31 00:10:28 +0900

    init

---
+++ coalive/src/main/java/com/cateam/coalive/CoaliveApplication.java
@@ -0,0 +1,15 @@
+package com.cateam.coalive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class CoaliveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoaliveApplication.class, args);
+    }
+
+}
---
+++ coalive/src/test/java/com/cateam/coalive/CoaliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cateam.coalive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoaliveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
